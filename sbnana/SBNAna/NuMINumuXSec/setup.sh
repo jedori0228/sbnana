307b4519857e708fb22d8b4bd85df68016f6b9f1
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2024-05-09 11:40:25 -0500

    reslient and persistent is full. use scratch for local tar

--- sbnana/SBNAna/NuMINumuXSec/setup.sh
+++ sbnana/SBNAna/NuMINumuXSec/setup.sh
@@ -15,7 +15,8 @@ export ANANAME=v09_75_03
 export gridJobDir=/pnfs/icarus/scratch/users/${USER}/NuMINumuXSec/GridJob/
 export gridBinDir=${gridWD}/bin/
 export gridLibDir=${gridWD}/lib/
-export gridLibDirPNFS=/pnfs/icarus/resilient/users/jskim/NuMINumuXSec/${ANANAME}/
+#export gridLibDirPNFS=/pnfs/icarus/resilient/users/jskim/NuMINumuXSec/${ANANAME}/
+export gridLibDirPNFS=/pnfs/icarus/scratch/users/jskim/NuMINumuXSec/tars/${ANANAME}/
 export gridDataDir=${gridWD}/data/
 export gridOutputDir=/pnfs/icarus/persistent/users/${USER}/NuMINumuXSec/
 
