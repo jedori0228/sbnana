b60cabfa8447f9ad3f73f54f51825ca69d5d8e5d
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2024-11-11 10:12:51 -0600

    Adding fitted version of Signal shape detector systematics

--- sbnana/SBNAna/Cuts/NuMIXSecDetectorSysts.cxx
+++ sbnana/SBNAna/Cuts/NuMIXSecDetectorSysts.cxx
@@ -25,14 +25,35 @@ namespace ana {
     else if(kDetSystType==kFrontIndPlaneNoise){
       // +1: reduce the rate by 10%
       //  0: CV
-      double this_sigma = sigma<0. ? 0. : sigma;
+      // negative is mirrored
+      double this_sigma = abs(sigma);
       weight *= 1. + this_sigma * (-0.10);
     }
     else if(kDetSystType==kFrontIndPlaneSignalShape){
       // +1: reduce the rate by 10%
       //  0: CV
-      double this_sigma = sigma<0. ? 0. : sigma;
+      // negative is mirrored
+      double this_sigma = abs(sigma);
       weight *= 1. + this_sigma * (-0.10);
+    }
+    else if(kDetSystType==kFrontIndPlaneSignalShapeFitted){
+      // :1: smoothstep function
+      //  0: CV
+      // negative is mirrored
+
+      double RecoProtonP = kNuMIProtonCandidateRecoP(sr);
+
+      double this_sigma = abs(sigma);
+
+      double this_stepfunc = GetSmoothStepFunction(
+        RecoProtonP,
+        -6.59065706e+01,
+        6.72272754e-01,
+        -2.05684515e+03,
+        2.05780820e+03
+      );
+      double this_onesig = abs(1.-this_stepfunc) * (-1.);
+      weight *= 1. + this_sigma * this_onesig;
     }
     else if(kDetSystType==kMiddleIndPlaneTransparency){
       weight *= 1. + sigma * 0.05;
@@ -48,26 +69,15 @@ namespace ana {
 
   }
 
-  const NuMIXSecDetectorSysts kNuMIXSecFrontIndPlaneGainSyst(
-    NuMIXSecDetectorSysts::kFrontIndPlaneGain,
-    "NuMIXSecFrontIndPlaneGainSyst",
-    "Front ind. plane gain #pm10%"
-  );
-  const NuMIXSecDetectorSysts kNuMIXSecFrontIndPlaneNoiseSyst(
-    NuMIXSecDetectorSysts::kFrontIndPlaneNoise,
-    "NuMIXSecFrontIndPlaneNoiseSyst",
-    "Front ind. plane noise +10%"
-  );
-  const NuMIXSecDetectorSysts kNuMIXSecFrontIndPlaneSignalShapeSyst(
-    NuMIXSecDetectorSysts::kFrontIndPlaneSignalShape,
-    "NuMIXSecFrontIndPlaneSignalShapeSyst",
-    "Front ind. plane signal shape"
-  );
-  const NuMIXSecDetectorSysts kNuMIXSecMiddleIndPlaneTransparencySyst(
-    NuMIXSecDetectorSysts::kMiddleIndPlaneTransparency,
-    "NuMIXSecMiddleIndPlaneTransparencySyst",
-    "Middle ind. plane transparency"
-  );
+  double NuMIXSecDetectorSysts::GetSmoothStepFunction(double x, double x_min, double x_max, double offset, double scale) const {
 
+    double X = (x - x_min) / (x_max - x_min);
+    if(X<0) X = 0.;
+    if(X>1.0) X = 1.0;
+
+    double out = (3.-2.*X)*(X*X);
+    return offset + scale * out;
+
+  }
 
 } // end namespace ana
